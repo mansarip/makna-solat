ae5c344722ae437bc017a08ebb2be85a70a5302d
Luqman <luqmanrasa@gmail.com>
2019-05-12 07:49:27 +0800

    Updates

---
+++ precache-manifest.9aafb1f2ec0dc9a0b18cf5a7e7d89bba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d8d7fd10f1a57897905fa982a40326f",
+    "url": "/makna-solat/index.html"
+  },
+  {
+    "revision": "6facd58e64580daaa8ce",
+    "url": "/makna-solat/static/css/main.ccb56227.chunk.css"
+  },
+  {
+    "revision": "fa725bf379f2ce99b75b",
+    "url": "/makna-solat/static/js/2.d49047e8.chunk.js"
+  },
+  {
+    "revision": "6facd58e64580daaa8ce",
+    "url": "/makna-solat/static/js/main.152e973f.chunk.js"
+  },
+  {
+    "revision": "d6f2321232198ec1dc4e",
+    "url": "/makna-solat/static/js/runtime~main.ef464305.js"
+  }
+]);
