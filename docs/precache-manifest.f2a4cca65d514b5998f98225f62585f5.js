0de2954dfb6b4058132eb4b58f08b7d28ea7ea94
Luqman <luqmanrasa@gmail.com>
2019-05-12 07:44:25 +0800

    test publish

---
+++ docs/precache-manifest.f2a4cca65d514b5998f98225f62585f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f94d45fd00bb287507537e4038776682",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d9a875ac56e1d2a0d1a",
+    "url": "/static/css/main.ccb56227.chunk.css"
+  },
+  {
+    "revision": "fa725bf379f2ce99b75b",
+    "url": "/static/js/2.d49047e8.chunk.js"
+  },
+  {
+    "revision": "7d9a875ac56e1d2a0d1a",
+    "url": "/static/js/main.b28f690f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
